c71fdfb30c319278be07f682d953475515de5f7e
AndreaLaura-23 <andrelaura1.al@gmail.com>
2025-12-31 10:37:22 +0000

    Fixed error in user_drive and tested

--- src/user_drive.cpp
+++ src/user_drive.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <sstream>
 #include <atomic>
+#include <utility>
 
 class UserDrive : public rclcpp::Node {
 public:
@@ -21,13 +22,20 @@ public:
     );
 
     RCLCPP_INFO(this->get_logger(),
-      "User drive ready: publishing on /cmd_vel_user. Service /get_avg_vel active.");
+       "=== ROBOT CONTROL INTERFACE ===\n"
+      "Publishing on /cmd_vel_user\n"
+      "Service /get_avg_vel active"
+      "Type 'q' to quit.");
 
     input_thread_ = std::thread([this]() { this->input_loop(); });
   }
 
   ~UserDrive() override {
     running_ = false;
+    
+    geometry_msgs::msg::Twist stop;
+    pub_->publish(stop);
+
     if (input_thread_.joinable()) input_thread_.join();
   }
 
@@ -46,41 +54,68 @@ private:
       return;
     }
 
-    double sum_lin = 0.0, sum_ang = 0.0;
-    for (auto &p : last_cmds_) {
+    double sum_lin = 0.0;
+    double sum_ang = 0.0;
+    for (const auto &p : last_cmds_) {
       sum_lin += p.first;
       sum_ang += p.second;
     }
-    res->avg_linear = static_cast<float>(sum_lin / n);
-    res->avg_angular = static_cast<float>(sum_ang / n);
+    res->avg_linear  = static_cast<float>(sum_lin / static_cast<double>(n));
+    res->avg_angular = static_cast<float>(sum_ang / static_cast<double>(n));
   }
 
   void input_loop() {
-    std::string line;
-    std::cout << "Enter: linear_vel angolar_vel (e.g., 0.2 0.8). Press Ctrl+C to exit.\n";
+     while (running_ && rclcpp::ok()) {
+      double v_lin = 0.0;
+      double v_ang = 0.0;
+     
+      // Linera velocity
+      while (running_ && rclcpp::ok()) {
+        std::cout << "\nAdd linear velocity: " << std::flush;
+        std::string s;
+        if (!std::getline(std::cin, s)) return;
+        if (s == "q" || s == "quit") { running_ = false; return; }
 
-    while (running_ && rclcpp::ok()) {
-      std::cout << "> " << std::flush;
-      if (!std::getline(std::cin, line)) break;
-      if (line.empty()) continue;
+        std::stringstream ss(s);
+        if (ss >> v_lin) break;
+        std::cout << "The value not valid, retry.\n";
+    }
+      // Angular Velocity
+      while (running_ && rclcpp::ok()) {
+        std::cout << "Add angular velocity: " << std::flush;
+        std::string s;
+        if (!std::getline(std::cin, s)) return;
+        if (s == "q" || s == "quit") { running_ = false; return; }
 
-      std::istringstream iss(line);
-      double v_lin, v_ang;
-      if (!(iss >> v_lin >> v_ang)) {
-        std::cout << "Invalid format. Example: 0.2 0.8\n";
-        continue;
+        std::stringstream ss(s);
+        if (ss >> v_ang) break;
+        std::cout << "The value not valid, retry.\n";
       }
-
-      geometry_msgs::msg::Twist msg;
-      msg.linear.x = v_lin;
-      msg.angular.z = v_ang;
-      pub_->publish(msg);
-
+      
       {
         std::lock_guard<std::mutex> lk(mtx_);
         last_cmds_.push_back({v_lin, v_ang});
         if (last_cmds_.size() > 5) last_cmds_.pop_front();
       }
+
+      geometry_msgs::msg::Twist msg;
+      msg.linear.x = v_lin;
+      msg.angular.z = v_ang;
+
+      std::cout << "\nSend command for 10 second...\n";
+
+      const auto start = this->now();
+      rclcpp::Rate rate(10); // 10 Hz (0.1s)
+      while (running_ && rclcpp::ok() && (this->now() - start).seconds() < 10.0) {
+        pub_->publish(msg);
+        rate.sleep();
+      }
+
+      geometry_msgs::msg::Twist stop;
+      pub_->publish(stop);
+      std::cout << "Robot stopped.\n";
+      std::cout << "You can insert a new command now!\n";
+
     }
   }
 
